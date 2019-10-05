f0a077edc1c4e072ceda6c5730b975f0cbc42ca1
Ricky <ballard.programmer@gmail.com>
2019-10-05 21:09:31 +0100

    Updated GuildTimer, and added Prices command
    Added Prices Command

--- commands/guildtimer.js
+++ commands/guildtimer.js
@@ -4,15 +4,12 @@ module.exports = {
 	cooldown: 5,
 	description: 'Command to work out and Monitor Guild Claim Timer',
 	run(bot, message) {
-        // User to input monument support points
-        // Using only messages that are numbers 
-
         // Create the DM channel so you can have the reference on the dmChannel - otherwise you cant await messages correctly - the message.channel in this context is your general channel or any other!!!
         message.author.createDM().then((userDmChannel) => {
             console.log("CREATED DM SUCCESSFULLY");
 
             // Send the message to the DM, but only AFTER that (in .then) start awaiting messages. That ensures that the bots own message isnt collected
-            userDmChannel.send("How many support points does your monument have?")
+            userDmChannel.send("How many support points does your monument currently have?")
             .then(() => {
                 // First create the awaiting of message
                 let supportPoint = message => parseInt(message.content) > 0;
@@ -21,7 +18,7 @@ module.exports = {
                     let userReply1 = collected.first();
                     // Same as before, first send the message and only after initiating wait for response
                     if ((userReply1%1) != 0 ){
-                       console.log("Letters Added - Timed out!");
+                       userDmChannel.send("Please only use Numbers!, You will now have to restart the command!");
                     }else {
                     userDmChannel.send("What is your support point cost per day?")
                     .then(() => {
@@ -30,7 +27,7 @@ module.exports = {
                         .then((cost) => {   
                             let userReply2 = cost.first();
                             if ((userReply1%1) != 0 ){
-                                console.log("Letters Added - Timed out!");
+                                userDmChannel.send("Please only use Numbers!, You will now have to restart the command!");
                             }else {
                             if (userReply1 && userReply2){
                                 let divide = userReply1 / userReply2;
---
+++ commands/price.js
@@ -0,0 +1,114 @@
+const Discord = require("discord.js");
+// Add the filesystem
+const fs = require('fs');
+module.exports = {
+	name: 'price',
+	// Timer to avoid Spam, Useful with API's
+	cooldown: 5,
+	description: 'Command to get market price data',
+	run(bot, message, args){
+		try{
+			// Define User
+			let user = message.author.username;
+			// Define Channels to be used:
+			const mark = bot.channels.get("630090190790066196");	// Market Request Channel
+			// Only accept requests in market channel
+				if (message.channel != mark) {
+					return;
+				}
+			// get the sales sheet
+			fs.readFile('./scraped_files/current_sales.json',
+				// callback function that's called when reading a file is done!
+				async function(err, data) {
+					// json data
+					let salesInfo = data;
+					// parse Json file
+					let sales = JSON.parse(salesInfo);
+					// set the loop area
+					let salesSheet = sales.data;
+					const salesData = sortbyCoinPrice(salesSheet);
+					salesData;
+				});
+			// Sort by coin price Lowest - Highest
+			async function sortbyCoinPrice(salesSheet){
+				let sortData = salesSheet;
+				sortData.sort(function(a, b){
+					return a.CoinPrice - b.CoinPrice;
+				});
+				const coinSortedSheet = filterbyInput(sortData);
+				await coinSortedSheet;
+			}
+
+			async function filterbyInput(sortData){
+				const sortedData = sortData;
+				await message.channel.send("What Item do you want market details on?");
+				const itemName = await response(sortedData);
+				filteredData = sortedData.filter(response => itemName.includes(response.Name));
+				let responded = await filteredData.map(response => (response));
+				const output = outputRecord(responded, itemName);
+				output;
+			}
+
+			async function response(sortedData){
+				const filter = m => sortedData.filter(respond => m.content.includes(respond.Name));
+				const collector = await message.channel.awaitMessages(filter, {time: 10000, max: 1});
+				return collector.first().content;
+			}
+
+			async function outputRecord(responded, itemName){
+				const pages = responded.length;
+				let page = 1;
+				for (let i = 0; i < responded.length; i++){
+					const item = responded[i];
+						if (i == page-1){
+							let embOutput = buildembOutput(item, user, page, pages, itemName);
+							message.channel.send(embOutput).then(msg => {
+								msg.react('◀').then( r => {
+									msg.react('▶')
+										// Filters
+										const prevFilter = (reaction, user) => reaction.emoji.name === '◀' && user.id === message.author.id;
+										const nextFilter = (reaction, user) => reaction.emoji.name === '▶' && user.id === message.author.id;
+										// Collectors
+										const previous = msg.createReactionCollector(prevFilter);
+										const next = msg.createReactionCollector(nextFilter);
+										// Manage Collections
+										previous.on('collect', r => {
+											if (page === 1) return; // If on first page, disable going back!
+											page--; // Not on first page, go back
+											embOutput = buildembOutput(responded[page-1], user, page, pages, itemName);
+											msg.edit(embOutput);
+											// removes reaction
+											r.remove(r.users.filter(u => u === message.author).first());
+										})
+										next.on('collect', r => {
+											if (page === pages) return; // If on last page, disable going forwards!
+											page++; // Not on last page, go forward
+											// embOutput.setFooter(`Page ${page} of ${pages} || © Lifguilds - A LIF:MMO Bot`);
+											embOutput = buildembOutput(responded[page-1], user, page, pages, itemName);
+											msg.edit(embOutput);
+											r.remove(r.users.filter(u => u === message.author).first());
+										})
+									})
+							});
+						}
+					}
+				}
+			function buildembOutput(item, user, page, pages, itemName){
+				const embOutput = new Discord.RichEmbed()
+							.setColor('#1eb7d9')
+							.setTitle(`Market Price for ${itemName}`)
+							.setDescription(`${user}`)
+							.addField(`__Item:__`, item.Name)
+							.addField(`__Quality:__`, item.Quality)
+							.addField(`__Quantity:__`, item.Quantity)
+							.addField(`__Price:__`, item.CoinPrice)
+							.addField(`__Tradepost__`, item.TradePostName)
+							.setThumbnail('https://i.imgur.com/hmWTeKv.png')
+							.setFooter(` Page ${page} of ${pages} || © Lifguilds - A LIF:MMO Bot`);
+							return embOutput;
+			}
+		}catch (error) {
+			console.error(error);
+		}
+	}		
+}
